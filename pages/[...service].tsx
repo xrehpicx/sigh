9c7cae83825d88e17c9539929dadd643f88aa3a9
raj <raj.fps2000@gmail.com>
2022-12-08 21:36:05 +0530

    fixes

--- pages/[...service].tsx
+++ pages/[...service].tsx
@@ -1,10 +1,13 @@
 import { HomeNav } from "components/HomeNav";
-import { MapItemType, WelcomePageProps } from "components/types";
+import { MarkdownComponents } from "components/MarkdownComponents";
+import { DocData, MapItemType } from "components/types";
 import { getDocsStruct, getDoc } from "lib/minio";
 import { BucketItem } from "minio";
 import { GetServerSideProps } from "next";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import { useMemo } from "react";
+import { createContext, useContext, useMemo } from "react";
+import pathListToTree, { TreeNode } from "path-list-to-tree";
 import ReactMarkdown from "react-markdown";
 
 interface IDocData {
@@ -17,13 +20,19 @@ export const getServerSideProps: GetServerSideProps = async function (context) {
   };
 
   const SerivceName = query.service[0];
-  console.log(query.service)
-  if(query.service[(query.service.length - 1)].includes("."))  {
-    query.service = query.service.slice(0, - 1);
-    console.log(query.service)
+  console.log(query.service);
+  if (query.service[query.service.length - 1].includes(".")) {
+    query.service = query.service.slice(0, -1);
+    console.log(query.service);
   }
-  const FilePath = query.service.length > 1 ? `/${query.service.slice(1, query.service.length + 1).join('/')}/`:''
-  const DocStruct: BucketItem[] | null = await getDocsStruct(SerivceName, FilePath);
+  const FilePath =
+    query.service.length > 1
+      ? `/${query.service.slice(1, query.service.length + 1).join("/")}/`
+      : "";
+  const DocStruct: BucketItem[] | null = await getDocsStruct(
+    SerivceName,
+    FilePath
+  );
   const promises = DocStruct
     ? DocStruct.map(async (item) => {
         const mapItem: MapItemType = [
@@ -48,6 +57,16 @@ export const getServerSideProps: GetServerSideProps = async function (context) {
   };
 };
 
+interface IServicePageContext {
+  serviceName: string;
+  DocMap: Map<string, DocData>;
+  getHomeData: () => DocData | null | undefined;
+  faviconUrl: string | undefined;
+}
+
+const ServicePageContext = createContext({} as IServicePageContext);
+const useServiceData = () => useContext(ServicePageContext);
+
 export default function Doc({ DocMapArr }: IDocData) {
   const router = useRouter();
   const query = router.query as {
@@ -71,7 +90,7 @@ export default function Doc({ DocMapArr }: IDocData) {
       serviceName,
       DocMap,
       getHomeData,
-      
+
       faviconUrl: favicon
         ? `http://${window.location.host}/api/static/${serviceName}/${favicon}`
         : favicon,
@@ -79,51 +98,65 @@ export default function Doc({ DocMapArr }: IDocData) {
   }, [DocMapArr, query.service]);
 
   return (
-    <main className="bg-white dark:bg-background-900 h-screen">
-      <HomeNav
-        serviceName={docData.serviceName}
-        faviconUrl={docData.faviconUrl}
-      />
-      <div>
-        <div className="max-w-8xl mx-auto px-4 sm:px-6 md:px-8">
-          <div className="hidden lg:block fixed z-20 inset-0 top-[3.8125rem] left-[max(0px,calc(50%-45rem))] right-auto w-[19.5rem] pb-10 px-8 overflow-y-auto">
-            <DocNav />
-          </div>
-          <div className="lg:pl-[19.5rem]">
-            <ReactMarkdown
-              className="text-background-900 dark:text-primary-50 max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16"
-              components={{
-                h1: ({ ...props }) => (
-                  <h1 {...props} className="text-6xl my-2" />
-                ),
-                h2: ({ ...props }) => (
-                  <h2 {...props} className="text-4xl my-2" />
-                ),
-                h3: ({ ...props }) => (
-                  <h3 {...props} className="text-2xl my-2" />
-                ),
-                li: ({ ...props }) => (
-                  <li {...props} className="list-disc ml-4" />
-                ),
-              }}
-              //need 404 page 
-            >
-              {docData.getHomeData() !=null ? docData.getHomeData()!.docString : "No such page Exists!!"}
-            </ReactMarkdown>
-
-            <RightDocNav />
+    <ServicePageContext.Provider value={docData}>
+      <main className="bg-white dark:bg-background-900 h-screen">
+        <HomeNav
+          serviceName={docData.serviceName}
+          faviconUrl={docData.faviconUrl}
+        />
+        <div>
+          <div className="max-w-8xl mx-auto px-4 sm:px-6 md:px-8">
+            <div className="hidden lg:block fixed z-20 inset-0 top-[3.8125rem] left-[max(0px,calc(50%-45rem))] right-auto w-[19.5rem] pb-10 px-8 overflow-y-auto">
+              <DocNav />
+            </div>
+            <div className="lg:pl-[19.5rem]">
+              <ReactMarkdown
+                className="text-background-900 dark:text-primary-50 max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16"
+                components={MarkdownComponents}
+              >
+                {
+                  docData.getHomeData() != null
+                    ? docData.getHomeData()!.docString
+                    : "No such page Exists!!"
+                  //need 404 page
+                }
+              </ReactMarkdown>
+              <RightDocNav />
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </ServicePageContext.Provider>
   );
 }
 
 function DocNav() {
+  const { DocMap, serviceName } = useServiceData();
+
+  const navData = useMemo(() => {
+    return Array.from(DocMap.keys()).filter((path) => path.includes(".md"));
+  }, [DocMap]);
+
+  console.log(pathListToTree(navData)[0].children);
+
   return (
     <nav className="lg:text-sm lg:leading-6 relative">
       <NavSearch />
-      <h1 className="text-primary-50 text-xl">Test Nav</h1>
+      {navData.map((path) => (
+        <div
+          key={path}
+          className="space-y-6 lg:space-y-2 border-l border-background-100 dark:border-background-800"
+        >
+          <Link
+            href={`${serviceName}/${path}`}
+            className="capitalize block border-l pl-4 -ml-px border-transparent hover:border-slate-400 dark:hover:border-slate-500 text-background-700 hover:text-slate-900 dark:text-background-200 dark:hover:text-slate-300"
+          >
+            {path.toLowerCase().endsWith("readme.md")
+              ? "Home"
+              : path.split("/")[1].split(".md")[0].replaceAll("-", " ")}
+          </Link>
+        </div>
+      ))}
     </nav>
   );
 }
@@ -140,6 +173,14 @@ function NavSearch() {
   return (
     <div className="sticky top-0 -ml-0.5 pointer-events-none">
       <div className="h-10" />
+    </div>
+  );
+}
+
+function ExpanableNavItem({ node }: { node: TreeNode }) {
+  return (
+    <div>
+      <span>{node.name}</span>
     </div>
   );
 }
